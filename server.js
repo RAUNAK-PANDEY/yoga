23aa9fa9415bdeca798288ad27b10f2b5e343dcf
Raunak Pandey <raunakpandey1007@gmail.com>
2022-12-12 00:03:54 +0530

    change5

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const PORT = process.env.PORT || 5000;
 
  
 
-  app.use(express.static(path.join(__dirname , "./client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
   app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+      res.sendFile(path.join(__dirname, "client/build/index.html"));
   })
 
 
