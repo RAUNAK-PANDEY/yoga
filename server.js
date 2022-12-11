454d23318d6f5f207806f8224fba0e65c8083a44
Raunak Pandey <raunakpandey1007@gmail.com>
2022-12-11 21:03:41 +0530

    change2

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const PORT = process.env.PORT || 8000;
 
 
  
-// if (process.env.NODE_ENV == "production"){
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV == "production"){
+  app.use(express.static(path.join(__dirname , "./client/build")));
   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
   })
-// }
+}
 
 
 const server = app.listen(PORT, () =>
