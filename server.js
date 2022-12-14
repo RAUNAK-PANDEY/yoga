fa26b5fa986b78b2f81aaeaee367cd782e61a279
Raunak Pandey <raunakpandey1007@gmail.com>
2022-12-14 23:31:50 +0530

    ch6

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build','index.html'));
 });
 
 
